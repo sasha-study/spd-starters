72b81ec881d2480a0be10e13de76539dfe5c12f0
Sasha <72855275+sasha-flow@users.noreply.github.com>
2024-12-13 10:57:42 +0200

    up to 5 is done

--- Week-2/problem_bank/rocket-error-starter.rkt
+++ Week-2/problem_bank/rocket-error-starter.rkt
@@ -3,10 +3,10 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname rocket-error-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require spd/tags)
 
-(@assignment htdd-p7)
-(@csid ??? ???)
-
-(@problem 1)
+;(@assignment htdd-p7)
+;(@csid ??? ???)
+;
+;(@problem 1)
 ;; Consider the following data definition from the Rocket practice problem.
 ;;
 ;; We have designed a function has-landed?, but there are errors in the function
@@ -15,47 +15,47 @@
 ;; consistent.
 
 
-;;; =================
-;;; Data Definitions: 
-;
+; =================
+;; Data Definitions: 
+
 ;(@htdd RocketDescent)
-;;; RocketDescent is one of:
-;;; - Number
-;;; - false
-;;; interp. false if rocket's descent has ended, otherwise number of kilometers
-;;;         left to Earth, restricted to (0, 100]
-;(define RD1 100)
-;(define RD2 40)
-;(define RD3 0.5)
-;(define RD4 false)
-;
-;(@dd-template-rules one-of              ;2 cases
-;                    atomic-non-distinct ;Number
-;                    atomic-distinct)    ;false
-;#;
-;(define (fn-for-rocket-descent rd)
-;  (cond [(number? rd)
-;         (... rd)]
-;         [else  (...)])) 
-;
-;
-;
-;;; =================
-;;; Functions:
-;
+;; RocketDescent is one of:
+;; - Number
+;; - false
+;; interp. false if rocket's descent has ended, otherwise number of kilometers
+;;         left to Earth, restricted to (0, 100]
+(define RD1 100)
+(define RD2 40)
+(define RD3 0.5)
+(define RD4 false)
+
+(@dd-template-rules one-of              ;2 cases
+                    atomic-non-distinct ;Number
+                    atomic-distinct)    ;false
+#;
+(define (fn-for-rocket-descent rd)
+  (cond [(number? rd)
+         (... rd)]
+         [else  (...)])) 
+
+
+
+;; =================
+;; Functions:
+
 ;(@htdf has-landed?)
 ;(@signature RocketDescent -> Boolean)
-;;; produce true if rocket's descent has ended; false if it's still descending
-;(check-expect (has-landed? 100) 100)
-;(check-expect (has-landed? 23) 23)
-;(check-expect (has-landed? 0.25) 0.25)
-;(check-expect (has-landed? false) true)
-;
-;;(define (has-landed? r) r) ; stub
-;
+;; produce true if rocket's descent has ended; false if it's still descending
+(check-expect (has-landed? 100) false)
+(check-expect (has-landed? 23) false)
+(check-expect (has-landed? 0.25) false)
+(check-expect (has-landed? false) true)
+
+;(define (has-landed? r) r) ; stub
+
 ;(@template RocketDescent)
-;
-;(define (has-landed? rd)
-;  (cond [(number? rd)
-;         false]
-;        [else true])) 
+
+(define (has-landed? rd)
+  (cond [(number? rd)
+         false]
+        [else true])) 
